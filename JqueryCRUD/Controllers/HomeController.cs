74fa8a8fdd4fc4bcbf9d780d86d9f39da6cba21a
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2021-07-12 13:52:29 +0500

    Intial Commit

---
+++ JqueryCRUD/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using JqueryCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryCRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        UserDBEntities DB = new UserDBEntities();
+     
+       
+        public ActionResult List()
+        {
+            var Data = DB.Users.ToList();
+            return View(Data);
+        }
+
+        [HttpPost]
+        public ActionResult Create(User u)
+        {
+            string message = "Saved Successfully";
+            bool status = true;
+            DB.Users.Add(u);
+            DB.SaveChanges();
+            return Json(new { status = status, message = message, id = DB.Users.Max(x => x.ID) }, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Delete(int Id)
+        {
+            var del = DB.Users.Where(x => x.ID == Id).FirstOrDefault();
+            DB.Users.Remove(del);
+            DB.SaveChanges();
+            string message = "Record has been deleted successfully.";
+            bool status = true;
+            return Json(new { status = status, message = message }, JsonRequestBehavior.AllowGet);
+
+        }
+        public ActionResult GetUsers(int ID)
+        {
+            User data = new User();
+            var getuser = DB.Users.Where(x => x.ID == ID).FirstOrDefault();
+            data.ID = getuser.ID;
+            data.Name = getuser.Name;
+            data.Age = getuser.Age;
+            data.Country = getuser.Country;
+            data.State = getuser.State;
+
+            return Json(new { success = true, data = data }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Update(User model)
+        {
+            DB.Entry(model).State = System.Data.Entity.EntityState.Modified;
+            DB.SaveChanges();
+            string message = "Recored has been updated successfully";
+            bool status = true;
+            return Json(new { status = status, message = message }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ JqueryCRUD/Scripts/CrudOpration.js
@@ -0,0 +1,140 @@
+﻿$('#usertable').DataTable({});
+
+// Code for Show PopUp for insert Value
+$('#btnAdd').click(function () {
+    $('#txtName').val('');
+    $('#txtAge').val('');
+    $('#txtCountry').val('');
+    $('#txtState').val('');
+    $('#ModalTitle').text('Add Users');
+    $('#btnUpdate').hide();
+    $('#UserModal').modal('show');
+});
+
+// Code For Save Value in Database
+
+$('#btnSave').click(function () {
+    var table = $('#usertable').DataTable()
+    if ($('#txtName').val() == '' ||
+        $('#txtAge').val() == '' ||
+        $('#txtCountry').val() == '' ||
+        $('#txtState').val() == '') {
+        toastr.error("All fields are required");
+        //toastr.options = {
+        //    "closeButton": true,
+        //    "debug": false,
+        //    "newestOnTop": false,
+        //    "progressBar": false,
+        //    "positionClass": "toast-top-right",
+        //    "preventDuplicates": false,
+        //    "onclick": null,
+        //    "showDuration": "300",
+        //    "hideDuration": "1000",
+        //    "timeOut": "5000",
+        //    "extendedTimeOut": "1000",
+        //    "showEasing": "swing",
+        //    "hideEasing": "linear",
+        //    "showMethod": "fadeIn",
+        //    "hideMethod": "fadeOut"
+        //}
+    }
+    else {
+        var UserObj = {
+            Name: $('#txtName').val(),
+            Age: $('#txtAge').val(),
+            Country: $('#txtCountry').val(),
+            State: $('#txtState').val(),
+
+        }
+        $.ajax({
+            url: '/Home/Create',
+            dataType: 'json',
+            type: 'POST',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(UserObj),
+            success: function (response) {
+                if (response.status) {
+                    toastr.success(response.message, "Message")
+                    table.row.add([$('#txtName').val(), $('#txtAge').val(), $('#txtCountry').val(), $('#txtState').val()]).draw(false);
+                   
+                }
+            }
+
+        });
+        $('#UserModal').modal('hide')
+
+    }
+
+});
+
+// Code For Delete Value in Database
+
+$('body').on('click', '.btnDelete', function () {
+    var $this = $(this);
+    alert('Are you sure you want to delete this record ?');
+    var table = $('#usertable').DataTable();
+    $.ajax({
+        url: "/Home/Delete",
+        data: { ID: $($this).attr("data-id") },
+        type: "Post",
+        success: function (response) {
+            toastr.error(response.message);
+            table.row($($this).parents('tr'))
+                .remove()
+                .draw();
+        }
+    });
+});
+
+// Code For Get Id From DataTable
+
+function GetUser(ID) {
+    $.ajax({
+        url: "/Home/GetUsers",
+        data: { ID: ID },
+        type: "Get",
+        contentType: "application/json;charset=UTF-8",
+        dataType: "Json",
+        success: function (result) {
+            $('#txtID').val(result.data.ID);
+            $('#txtName').val(result.data.Name);
+            $('#txtAge').val(result.data.Age);
+            $('#txtCountry').val(result.data.Country);
+            $('#txtState').val(result.data.State);
+            $('#btnSave').hide();
+            $('#btnUpdate').show();
+            $('#ModalTitle').text("Update Users");
+            $('#UserModal').modal('show');
+        }
+    })
+}
+
+// Code for Update Data 
+
+function UpdateUser() {
+    var table = $('#usertable').DataTable();
+    var model = {
+        ID: $('#txtID').val(),
+        Name: $('#txtName').val(),
+        Age: $('#txtAge').val(),
+        Country: $('#txtCountry').val(),
+        State: $('#txtState').val()
+
+    }
+    $.ajax({
+        url: "/Home/Update",
+        type: "Post",
+        data: JSON.stringify(model),
+        contentType: "application/json; charset=UTF-8",
+        success: function (response) {
+            toastr.success(response.message)
+            table.row('selected').cell(':eq(0)').data($('#txtName').val()).draw();
+            table.row('selected').cell(':eq(1)').data($('#txtAge').val()).draw();
+            table.row('selected').cell(':eq(2)').data($('#txtCountry').val()).draw();
+            table.row('selected').cell(':eq(3)').data($('#txtState').val()).draw();
+
+            $('#UserModal').modal('toggle');
+
+        }
+    })
+}
